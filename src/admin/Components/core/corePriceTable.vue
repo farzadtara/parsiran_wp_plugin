be3f27d33361ddafd2bfc4a48aa22b45db980286
farzad <farzad.tara@gmail.com>
2025-01-17 22:54:10 +0330

    use core price table

---
+++ src/admin/Components/core/corePriceTable.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="w-full mt-3">
+        <span class="block text-xl font-bold mb-2">{{ props.category.name }} </span>
+        <div class="w-full border border-slate-200 rounded-xl overflow-x-auto">
+            <table class="w-full divide-y divide-x divide-slate-200 border-collapse border m-0">
+            <thead class="bg-amber-300 text-slate-800">
+                <tr>
+                <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">سکه بانکی</th>
+                <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">خرید</th>
+                <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">فروش</th>
+                </tr>
+            </thead>
+            <tbody
+                class="divide-y divide-x divide-slate-200 bg-white text-slate-800"
+            >
+                <tr
+                v-for="product of props.products"
+                :key="product.id"
+                class=" cursor-pointer odd:bg-white even:bg-slate-50"
+                >
+                <td class="px-4 py-2 border border-gray-300">{{ product.name }}</td>
+                <td class="px-4 py-2 border border-gray-300">{{ product.priceBuy }}</td>
+                <td class="px-4 py-2 border border-gray-300">{{ product.priceSell }}</td>
+                </tr>
+            </tbody>
+            </table>
+        </div>
+        </div>
+</template>
+
+<script setup>
+import { ref, watch } from 'vue' 
+
+
+const props = defineProps({
+    products:{
+        type: Array,
+        required: true
+    },
+    category:{
+        type: String,
+        required: true
+    }
+})
+
+
+
+
+
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/admin/pages/PriceTable.vue
+++ src/admin/pages/PriceTable.vue
@@ -1,61 +1,61 @@
 <template>
-  <div>
-    <div class="w-full border border-slate-200 rounded-xl overflow-x-auto">
-      <table class="w-full divide-y divide-x divide-slate-200 border-collapse border">
-        <thead class="bg-amber-300 text-slate-800">
-          <tr>
-            <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">سکه بانکی</th>
-            <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">خرید</th>
-            <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">فروش</th>
-          </tr>
-        </thead>
-        <tbody
-          class="divide-y divide-x divide-slate-200 bg-white text-slate-800"
-        >
-          <tr
-            v-for="product of products"
-            :key="product.id"
-            class=" cursor-pointer odd:bg-white even:bg-slate-50"
-          >
-            <td class="px-4 py-2 border border-gray-300">{{ product.name }}</td>
-            <td class="px-4 py-2 border border-gray-300">{{ product.priceBuy }}</td>
-            <td class="px-4 py-2 border border-gray-300">{{ product.priceSell }}</td>
-          </tr>
-        </tbody>
-      </table>
+  <div id="price-table-container" class="w-full">
+
+    <div v-for="(category, index) of categories" :key="index">
+      <corePriceTable :products="products[category.id]" :category="category" />
     </div>
+
+
   </div>
+
 </template>
 
 <script>
 import AJAX from "../Bits/AJAX";
 import { ToPersianMoney } from "../utils";
+import corePriceTable from "../Components/core/corePriceTable.vue";
 
 export default {
+  components: { corePriceTable },
   data() {
     return {
-      products: [],
+      products: {},
       fetchInterval: null,
+      categories: [],
     };
   },
 
-  // async beforeCreate() {
+  async beforeCreate() {
+    // const appElement = document.getElementById('WPWVT_app');
+    // console.log("appElement", appElement)
+    // if (appElement && appElement.dataset.categories) {
+    //   console.log('appElement :>> ', appElement);
+    //     console.log('JSON.parse(appElement.dataset.categories) :>> ', JSON.parse(appElement.dataset.categories));
+    //     this.categories = JSON.parse(appElement.dataset.categories);
 
-  // },
+    //   // try {
 
-  created() {
-    this.fetchAndProcessProducts();
+    //   // } catch (error) {
+    //   //   console.error("Error parsing JSON:", error);
+    //   // }
+    // }
+
+  },
+
+
+  async created() {
+    await this.fetchCategories()
+    await this.fetchAndProcessProducts()
+
   },
 
   mounted() {
     window.setTimeout(() => {
-      this.fetchInterval = setInterval(this.fetchAndProcessProducts, 30000);
+      this.fetchInterval = setInterval(this.fetchAndProcessProducts, 35000);
     }, 25000);
   },
 
   beforeDestroy() {
-    console.log("beforeDestroy");
-
     clearInterval(this.fetchInterval);
   },
 
@@ -67,40 +67,50 @@ export default {
       console.log("onRowReorder");
     },
 
-    async fetchAndProcessProducts() {
-      const rawProducts = await AJAX.get("product/all");
-
-      const processedProducts = Array.from(rawProducts)
-        .filter((product) => !product.isHidden)
-        .map((product) => {
-          if (product.isDisable) {
-            product.priceSell = 0;
-            product.priceBuy = 0;
-            return product;
-          }
-          return product;
-        })
-        .map((product) => {
-          if (!!product.isBuyDisable) {
-            product.priceBuy = null;
-            return product;
-          }
-          if (product.isSellDisable) {
-            product.priceSell = null;
-            return product;
-          }
-          return product;
-        })
-        .map((product) => {
-          if (product.priceSell)
-            product.priceSell = ToPersianMoney(product.priceSell);
-          if (product.priceBuy)
-            product.priceBuy = ToPersianMoney(product.priceBuy);
-          return product;
-        });
-
-      this.products = processedProducts;
+    async fetchCategories() {
+      const data = await AJAX.get("category/all");
+      this.categories = data.filter((item)=> !item.isHidden )
     },
+
+    async fetchAndProcessProducts() {
+      this.categories.forEach(async(category) => {
+        
+        const rawProducts = await AJAX.get(`product/category/${category.id}`);
+  
+        const processedProducts = Array.from(rawProducts)
+          .filter((product) => !product.isHidden)
+          .map((product) => {
+            if (product.isDisable) {
+              product.priceSell = 0;
+              product.priceBuy = 0;
+              return product;
+            }
+            return product;
+          })
+          .map((product) => {
+            if (!!product.isBuyDisable) {
+              product.priceBuy = "---";
+              return product;
+            }
+            if (product.isSellDisable) {
+              product.priceSell = "---";
+              return product;
+            }
+            return product;
+          })
+          .map((product) => {
+            if (product.priceSell)
+              product.priceSell = ToPersianMoney(product.priceSell);
+            if (product.priceBuy)
+              product.priceBuy = ToPersianMoney(product.priceBuy);
+            return product;
+          });
+  
+        this.products = {...this.products, [category.id]: processedProducts};
+      })
+
+    },
+
   },
 };
 </script>
