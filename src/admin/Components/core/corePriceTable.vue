d239a77940770edde18473415c5cf75f1d56a228
farzad <farzad.tara@gmail.com>
2025-01-18 00:01:12 +0330

    add full screen

--- src/admin/Components/core/corePriceTable.vue
+++ src/admin/Components/core/corePriceTable.vue
@@ -1,10 +1,14 @@
 <template>
-    <div class="w-full mt-3">
+    <div :class="{
+            'w-full max-w-4xl mx-auto p-4': !isFullScreen,
+            'fixed inset-0 bg-white z-50 p-4': isFullScreen
+        }" 
+    >
         <span class="block text-xl font-bold mb-2">{{ props.category.name }} </span>
         <div class="w-full border border-slate-200 rounded-xl overflow-x-auto">
             <table class="w-full divide-y divide-x divide-slate-200 border-collapse border m-0">
             <thead class="bg-amber-300 text-slate-800">
-                <tr>
+                <tr @dblclick="isFullScreen = !isFullScreen">
                 <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">سکه بانکی</th>
                 <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">خرید</th>
                 <th class="px-4 py-2 bg-yellow-400 text-start border border-gray-300">فروش</th>
@@ -31,6 +35,7 @@
 <script setup>
 import { ref, watch } from 'vue' 
 
+const isFullScreen = ref(false) 
 
 const props = defineProps({
     products:{
